90c9083a0284d187cd2007594fc31e85c54c8139
mohamed_haroon <moharoon11107@gmail.com>
2024-10-17 21:58:45 +0530

    Added SpringMail

---
+++ src/main/java/dev/haroon/controller/EmailController.java
@@ -0,0 +1,26 @@
+package dev.haroon.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.haroon.dto.EmailDTO;
+import dev.haroon.service.EmailService;
+
+@RestController
+@RequestMapping("/api")
+public class EmailController {
+	
+	    @Autowired
+	    private EmailService emailService;
+
+	    @PostMapping("/send-email")
+	    public ResponseEntity<String> sendEmail(@RequestBody EmailDTO emailDTO) {
+	        emailService.sendSimpleEmail(emailDTO.getFrom(), emailDTO.getTo(), emailDTO.getSubject(), emailDTO.getMessage());
+	        return ResponseEntity.ok("Email sent successfully!");
+	    }
+
+}
---
+++ src/main/java/dev/haroon/dto/EmailDTO.java
@@ -0,0 +1,20 @@
+package dev.haroon.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmailDTO {
+	
+	private String from;
+	
+	private String to;
+	
+	private String subject;
+	
+	private String message;
+
+}
